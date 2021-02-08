05274ed70897ec26ae6b7ce3a9fee4bff40c4154
azwarbahar07 <azwarbahar07@gmail.com>
2021-02-08 18:55:56 +0800

    create Rest Api

---
+++ api/order/addOrder.php
@@ -0,0 +1,38 @@
+<?php
+ require_once '../../koneksi.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $myir = $_POST['myir'];
+    $nama_lengkap= $_POST['nama_lengkap'];
+    $email = $_POST['email'];
+    $telpon = $_POST['telpon'];
+    $alamat = $_POST['alamat'];
+    $sto = "PNK-ANT";
+    $status_order = "PROCCESS";
+    $paket_id = $_POST['paket_id'];
+    $sales_id = $_POST['sales_id'];
+    $teknisi_id = null;
+    $mitra_id = null;
+
+    $query = "INSERT INTO tb_order values('','$myir',
+                                            '$nama_lengkap',
+                                            '$email',
+                                            '$telpon',
+                                            '$alamat',
+                                            '$sto',
+                                            '$status_order',
+                                            null,
+                                            '$paket_id',
+                                            '$sales_id',
+                                            '$teknisi_id',
+                                            '$mitra_id')";
+
+    $exeQuery =  mysqli_query($conn, $query);
+    echo ($exeQuery) ? json_encode(array('kode' =>1, 'pesan' => 'Berhasil Menambahkan Data'
+    )) : json_encode(array('kode' => 2, 'pesan' => 'Data Gagal Ditambahkan'));
+} else {
+    echo json_encode(array(' kode ' =>101, ' pesan ' => ' Request Tidak Valid' ));
+}
+
+?>
