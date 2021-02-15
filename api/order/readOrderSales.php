7f7d4c72983fd9414d15a8c13f69e5f7d09d5737
azwarbahar07 <azwarbahar07@gmail.com>
2021-02-15 16:19:53 +0800

    created Rest API for Mobile

---
+++ api/order/readOrderSales.php
@@ -0,0 +1,19 @@
+<?php
+ require_once '../../koneksi.php';
+
+ $status_order = $_GET["status_order"];
+ $sales_id = $_GET["sales_id"];
+
+ $query = "SELECT * FROM tb_order WHERE status_order='$status_order' AND sales_id='$sales_id' ORDER BY id_order DESC";
+
+ $result = mysqli_query($conn, $query);
+
+ $array = array();
+ while($row = mysqli_fetch_assoc($result)){
+    $array[] = $row;
+ }
+
+ echo ($result) ?
+ json_encode(array("kode" => 1, "order" => $array)) :
+ json_encode(array("kode" => 0, "pesan" => "Data tidak ditemukan"));
+?>
--- api/paket/getPaketId.php
+++ api/paket/getPaketId.php
@@ -1,16 +1,17 @@
 <?php
  require_once '../../koneksi.php';
 
- $query = "SELECT * FROM tb_paket";
+ $id_paket = $_GET["id_paket"];
+ $query = "SELECT * FROM tb_paket WHERE id_paket = '$id_paket' ";
 
  $result = mysqli_query($conn, $query);
 
- $array = array();
+//  $array = array();
  while($row = mysqli_fetch_assoc($result)){
-     $array[] = $row;
+     $array = $row;
  }
 
  echo ($result) ?
- json_encode(array("kode" => 1, "paket" => $array)) :
+ json_encode(array("kode" => 1, "result_paket_id" => $array)) :
  json_encode(array("kode" => 0, "pesan" => "Data tidak ditemukan"));
 ?>
