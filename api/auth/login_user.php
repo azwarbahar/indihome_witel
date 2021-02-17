fc5b98db9aaf0e80956a1eefb29f9f87cd0b676f
azwarbahar07 <azwarbahar07@gmail.com>
2021-02-17 18:46:21 +0800

    update rest api login

--- api/auth/login_user.php
+++ api/auth/login_user.php
@@ -1,18 +1,19 @@
 <?php
  require_once '../../koneksi.php';
-
  $username_auth = $_GET["username_auth"];
  $password_auth = $_GET["password_auth"];
  $ray = array();
  $query = "SELECT * FROM tb_auth WHERE username_auth='$username_auth'";
  $sql = mysqli_query($conn, $query);
- if ($sql){
-    $row_pass = mysqli_fetch_assoc($sql);
+ $row_pass = mysqli_fetch_assoc($sql);
+ if ($row_pass){
     if (password_verify($password_auth, $row_pass["password_auth"])) {
-            $ray=$row_pass;
+        $ray=$row_pass;
+        echo json_encode(array("kode" => 1, "auth_user" => $ray));
+    } else{
+        echo json_encode(array("kode" => 2, "pesan" => "Password tidak sesuai"));
     }
-    echo json_encode($ray);
-    mysqli_close($conn);
+ } else {
+    echo json_encode(array("kode" => 0, "pesan" => "Username tidak ditemukan"));
  }
-
 ?>
